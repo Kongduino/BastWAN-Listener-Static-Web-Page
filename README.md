023e938feda39464cd89e8586082419a4b5be373
Kongduino <dbvic@me.com>
2021-07-19 19:10:21 +0800

    Added dygraphs.py

--- README.md
+++ README.md
@@ -2,4 +2,9 @@
 
 See https://kongduino.wordpress.com/2021/07/15/indoors-environment-monitoring-with-lora-part-4/
 
+## Update 2021/07/19
+
+Added a Python version that runs off a Raspberry Pi (or any Python enable machine) with a RAK811 module connected via USB.
+
+See https://twitter.com/Kongduino/status/1417024046336643074 for the moment – it will also be document in Part 5.
 
---
+++ dygraphs.py
@@ -0,0 +1,50 @@
+import json
+import serial
+import time
+import sys
+import binascii
+import base64
+import hashlib
+from Crypto import Random
+from Crypto.Cipher import AES
+import datetime
+
+ser = serial.serial_for_url(str(sys.argv[1]), 115200)
+try:
+  if ser.isOpen():
+    print("\nReady\n===========\n")
+    ser.write(b'at+set_config=lorap2p:865000000:10:0:1:8:20\n')
+    print(b'at+set_config=lorap2p:865000000:10:0:1:8:20\n')
+    while True:
+      z=ser.read_until()
+      b=z.decode("utf-8").split("=")
+      cmd=b[0]
+      if cmd=="at+recv":
+        print(z)
+        c=b[1].split(':')
+        data=c[0].split(',')
+        rssi=data[0]
+        snr=data[1]
+        length=data[2]
+        #print("Decoding "+c[1])
+        enc=binascii.unhexlify(c[1].strip())
+        cipher = AES.new(b'YELLOW SUBMARINEENIRAMBUS WOLLEY', AES.MODE_ECB)
+        msg=cipher.decrypt(enc[0:len(enc)-28]).decode('utf-8')
+        msg=msg.split("}")[0]+"}"
+        try:
+          x=json.loads(msg)
+          dt= datetime.datetime.now()
+          S=f"{dt.strftime('%Y-%m-%d %H:%M:%S')},{x['H']},{x['T']},{x['V']},{x['C']}"
+          f = open("/var/www/html/dygraphs/data.csv", "a")
+          f.write(S)
+          f.write("\n")
+          print(S)
+          f.close()
+        except ValueError:  # includes simplejson.decoder.JSONDecodeError
+          print("Decoding JSON has failed")
+
+except serial.SerialException as e:
+  print("Exception")
+  sys.stderr.write('could not open port {!r}: {}\n'.format(args.port, e))
+  if ser.isOpen():
+    ser.close()
